8b2eb662ffd131510c1b9da8e689e83780da502c
Nicolas Wilmot <wilmotni@gmail.com>
2015-05-24 00:27:29 -0400

    Updates to work page, added box shadows

---
+++ js/thumb-hover.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+$('#eclectic-strength').on('mouseover', function() {
+	$('#prj1-hover').toggle();
+});
+$('#eclectic-strength').on('mouseout', function() {
+	$('#prj1-hover').toggle();
+});
+$('#interactive-list').on('mouseover', function() {
+	$('#prj2-hover').toggle();
+});
+$('#interactive-list').on('mouseout', function() {
+	$('#prj2-hover').toggle();
+});
+$('#dotirome').on('mouseover', function() {
+	$('#prj3-hover').toggle();
+});
+$('#dotirome').on('mouseout', function() {
+	$('#prj3-hover').toggle();
+});
+});
